f8c27cde3ae92c948950c9cf2d591b2f895cf13b
Hossein Maserrat <hmaserrat@gmail.com>
2013-09-20 13:14:45 -0700

    updated config files

---
+++ _ipythonrc.py
@@ -0,0 +1,4 @@
+import time, os, sys, couchdbkit, couchdb
+nodes = {'ip-10-251-25-221': 'ec2-54-212-217-209.us-west-2.compute.amazonaws.com', 'ip-10-249-69-157': 'ec2-54-212-181-88.us-west-2.compute.amazonaws.com', 'ip-10-251-96-56': 'ec2-54-218-168-160.us-west-2.compute.amazonaws.com', 'ip-10-250-35-223': 'ec2-54-212-171-150.us-west-2.compute.amazonaws.com'}
+sys.path.insert(0, '/home/hm/repos/p4home/work/tuba/disco/')
+

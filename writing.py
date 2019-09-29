1938a859f2f682d0c892c8e0aaaf127e1fc87bb3
hmas <hmaserrat@gmail.com>
2019-09-29 10:03:35 -0700

    Update writing.py

--- writing.py
+++ writing.py
@@ -1,4 +1,10 @@
-               'parameters': {'*': 0 if cutoff_date != '2018-01-01' else 1},
+desc = {
+        '_type': 'Ranking',
+        'rank': [
+            {
+                '_type': 'StateMachine',
+                'condition': num_names > 3 and buy > 0 and sell > 0,
+                'parameters': {'*': 0 if cutoff_date != '2018-01-01' else 1},
                 'order': ['s1', 's1-2', 's2', 's3', 's4'],
                 's1': [
                     ' '.join([
@@ -11,7 +17,7 @@
                     ]),
                 ],
                 's1-2': [
-                    ' according |{{to reports being filed with|records of}} Security Exchange Commission|{{ (SEC)|}}',
+                    ' according to |{{reports being filed with|records of}} Security Exchange Commission|{{ (SEC)|(www.sec.gov)|}}.',
                 ]
                 's2': [
                     '!{{Collectively|All together}}, insiders of {} |{{bought|purchased|acquired}} {} number of shares in the same period.'.format(company_name, buyers_collective_num_shares),
@@ -30,4 +36,22 @@
                         'signal, by an insider, more |{{meaningful|significant|newsworthy}} comparing to a ^{{sell|disposition}} signal.',
                     ])
                 ],
-            
+            },
+            {
+                '_type': 'StateMachine',
+                'condition': float(d[_k.SHARES_QUANTITY]) >= 1000 and d[_k.ACQUIRED_OR_DISPOSED] == 'D',
+                'start': '{} Dumps {} Shares of ${{{}}}'.format(nameformat(d[_k.OWNER_NAME]), sharequantityformat(d[_k.SHARES_QUANTITY]), _k.COMPANY_NAME),
+
+            },
+            {
+                '_type': 'StateMachine',
+                'condition': float(_total_value(d)) >= 1000000 and d[_k.ACQUIRED_OR_DISPOSED] == 'D',
+                'start': '{} Sells ${{{}}} shares of ${{{}}}'.format(nameformat(d[_k.OWNER_NAME]), _k.SHARES_QUANTITY, _k.COMPANY_NAME),
+            },
+            {
+                '_type': 'StateMachine',
+                'totalvalue': total_value(d),
+                'start': 'Insider Action Alert: ${{{}}}'.format(_k.COMPANY_NAME),
+            },
+        ],
+    }
